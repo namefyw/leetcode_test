3bb181cb29936d8e232b95de53fa1794c1e9bb36
fengyuanwei <fengyuanwei@ryeex.com>
2024-04-26 15:57:08 +0800

    简单20-21

--- leetcode.c
+++ leetcode.c
@@ -1,6 +1,70 @@
 
 #include "leetcode.h"
 
+#if leetcode_20
+char pairs(char a) {
+    if (a == '}') return '{';
+    if (a == ']') return '[';
+    if (a == ')') return '(';
+    return 0;
+}
+
+bool isValid(char* s) {
+    int n = strlen(s);
+    if (n % 2 == 1) {
+        return false;
+    }
+    int stk[n + 1], top = 0;
+    for (int i = 0; i < n; i++) {
+        char ch = pairs(s[i]);
+        if (ch) {
+            if (top == 0 || stk[top - 1] != ch) {
+                return false;
+            }
+            top--;
+        } else {
+            stk[top++] = s[i];
+        }
+    }
+    return top == 0;
+}
+
+#endif
+
+#if leetcode_21
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+#if 1 //实体指针法
+struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
+    if (list1 == NULL) {
+        return list2;
+    }
+    if (list2 == NULL) {
+        return list1;
+    }
+    struct ListNode head;
+    struct ListNode* tmp = &head;
+    while (list1 && list2) {
+        if (list1->val < list2->val) {
+            tmp->next = list1;
+            list1 = list1->next;
+        } else {
+            tmp->next = list2;
+            list2 = list2->next;
+        }
+        tmp = tmp->next;
+    }
+    tmp->next = (list1 != NULL) ? list1 : list2;
+    return head.next;
+}
+#endif
+#endif
+
 #if leetcode_219
 typedef struct record{
   int data;
--- leetcode.h
+++ leetcode.h
@@ -13,9 +13,11 @@
 #define q_err                           false   //解题有误
 
 /*Question number*/
+#define leetcode_20                     0
+#define leetcode_21                     1 // 合并两个有序链表
 #define leetcode_219                    0 // 存在重复元素 II
 #define leetcode_1793                   0 // 好子数组的最大分数
-#define leetcode_2007                   1 // 从双倍数组中还原原数组
+#define leetcode_2007                   0 // 从双倍数组中还原原数组
 #define leetcode_2312                   0 // 卖木头块
 #define leetcode_2642                   0 // 设计可以求最短路径的图类
 #define leetcode_2671                   0 // 频率跟踪器
